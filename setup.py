b0b7a87bab85a34155c8e8164f4bda81cb2ee4e0
Michael Chatten <chattenm@gmail.com>
2018-03-13 17:50:19 -0400

    changed version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='visionutils',
-	version='0.2.1',
+	version='0.2.2',
 	description='a colection of camera and image helper functions for working machine vision',
 	url='http://github.com/mchatten/visionutils',
 	author='Michael Chatten',
